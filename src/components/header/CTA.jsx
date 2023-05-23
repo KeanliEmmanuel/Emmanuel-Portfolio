cf58600881b6b2ff9303c8a6734b49b2f85a27a8
KeanliEmmanuel <business002001@gmail.com>
2023-05-23 01:08:45 +0100

    New commit

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+//import CV from '.././../assets/Akengni new.docx.pdf'
+
+const CTA = () => {
+    return (
+        <div className='cta'>
+            
+            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+            
+        </div>
+    )
+}
+
+export default CTA
---
+++ src/components/testimonial/Testimonial.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./testimonials.css";
+import IMG from "../../assets/3.jpg";
+import AV1 from "../../assets/3.jpg";
+import AV2 from "../../assets/3.jpg";
+import AV3 from "../../assets/3.jpg";
+import AV4 from "../../assets/3.jpg";
+import AV5 from "../../assets/3.jpg";
+
+// import Swiper core and required modules
+import { Pagination, Navigation, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+export const Testimonial = () => {
+  const testimonials = [
+    
+  ];
+  return (
+    <section id="testimonial">
+      <h5>Review from client</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container"
+      modules={[Pagination, Navigation, Scrollbar, A11y]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      >
+        {testimonials.map((testi) => {
+          return (
+            <SwiperSlide key={testi.id} className="testimonial">
+              <div className="client__avatar">
+                <img src={testi.avatar} alt="client avatar" />
+              </div>
+              <h5 className="client__name"> {testi.Cname} </h5>
+              <small className="client__review"> {testi.testimonial} </small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
+export default Testimonial;
